50ac8d05b8b71a5eeebfb30b5e6e9078dd0e1bbc
Gurpreeet <hgopy21@gmail.com>
2024-02-05 11:11:59 +0530

    new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,3 @@
-// let exitModal = document.getElementById('exitModal');
-// document.addEventListener('mouseleave',function(e){
-// exitModal.style.display = 'block'
-// document.body.style.overflow = "hidden";
-// })
-
-// let closeModal = document.querySelector('.closeBtn');
-// closeModal.addEventListener('click',function(){
-//   exitModal.style.display = 'none'
-// document.body.style.overflow = "auto";
-
-// })
 modalCount = 0;
 maxshow = 3
 let exitModal = document.getElementById('exitModal');
@@ -19,7 +7,6 @@ closeModal.addEventListener('click',function(){
   exitModal.style.display = 'none'
 document.body.style.overflow = "auto";
 })
-
 
 if(window.innerWidth > 768){
 console.log("desktop")
@@ -32,25 +19,22 @@ document.body.style.overflow = "hidden";
   }}
 })
 }
-
 else
 { 
-
-  alert('mbl1')
+  alert('mbl2')
  window.addEventListener('popstate', function(event) {
         modalCount++
 if(modalCount <=maxshow){
 exitModal.style.display = 'block'
 document.body.style.overflow = "hidden";
-  alert('mbl2')
+  alert('mbl3')
   }
-    });
-
-    history.pushState({ page: 1 }, "title 1", "#");
+  history.pushState({ page: 1 }, "title 1", "#");
 
     history.pushState(null, "", location.href);
     window.onpopstate = function () {
       history.go(1);
     };
-
+    });
+  
 }
